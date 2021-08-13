363013d9aaa17a48cc5664b372673c2b5cc4644e
Panagiotis Liakos <liakospanagiotis@gmail.com>
2021-08-13 07:57:09 +0300

    added test wind ABBY

--- tsdb/engine/tsm1/encoding_test.go
+++ tsdb/engine/tsm1/encoding_test.go
@@ -2163,13 +2163,55 @@ func TestEncoding_Stocks_USA_All(t *testing.T) {
 
 }
 
-
 func TestEncoding_Temp_Air_FNLT(t *testing.T) {
 	size := 1000
 	layout := "2006-01-02T15:04:05Z"
 	values := make([]tsm1.Value, size)
 
 	f, err := os.Open("/run/media/panagiotis/DATA/Stocks/NEON_temp-air-buoy-FNLT.csv.gz")
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer gz.Close()
+	scanner := bufio.NewScanner(gz)
+	currentRow := 0
+	totalSize := 0
+	totalTime := time.Duration(0)
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ",")
+		t, err := time.Parse(layout, row[0])
+		if err != nil {
+			fmt.Println(err)
+		}
+		if value, err := strconv.ParseFloat(row[1], 64); err == nil {
+			values[currentRow] = tsm1.NewValue(t.UnixNano(), value)
+			//fmt.Printf("%d: %v\n", t.UnixNano(), value)
+		}
+		currentRow += 1
+		if currentRow == size {
+			currentRow = 0
+			start := time.Now()
+			if b, err := tsm1.Values(values).Encode(nil); err == nil {
+				//fmt.Println(len(b))
+				totalSize += len(b)
+			}
+			elapsed := time.Since(start)
+			totalTime += elapsed
+		}
+	}
+
+	fmt.Printf("Total size: %v, Execution took %s\n", totalSize, totalTime)
+
+}
+
+func TestEncoding_Wind_ABBY(t *testing.T) {
+	size := 1000
+	layout := "2006-01-02T15:04:05Z"
+	values := make([]tsm1.Value, size)
+
+	f, err := os.Open("/run/media/panagiotis/DATA/Stocks/ABBY.csv.gz")
 	defer f.Close()
 	gz, err := gzip.NewReader(f)
 	if err != nil {
