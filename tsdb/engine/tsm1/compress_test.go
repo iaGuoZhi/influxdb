2c3d9ad450ea51ce58b1e59c61bfae0295b942fd
Panagiotis Liakos <liakospanagiotis@gmail.com>
2021-11-14 07:30:15 +0200

    new tests

--- tsdb/engine/tsm1/compress_test.go
+++ tsdb/engine/tsm1/compress_test.go
@@ -577,7 +577,6 @@ func TestCompress_Temp_BioMean(t *testing.T) {
 
 }
 
-
 func TestCompress_Size_Dust_Particulate_PM10Median(t *testing.T) {
 	size := 1000
 	layout := "01/02/2006 15:04:05"
@@ -629,6 +628,358 @@ func TestCompress_Size_Dust_Particulate_PM10sub50RHMedian(t *testing.T) {
 	values := make([]tsm1.Value, size)
 
 	f, err := os.Open("../../../NEON_size-dust-particulate-PM10sub50RHMedian.csv.gz")
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer gz.Close()
+	scanner := bufio.NewScanner(gz)
+	currentRow := 0
+	totalSize := 0
+	totalTime := time.Duration(0)
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ",")
+		t, err := time.Parse(layout, fmt.Sprintf("%s %s", row[0], row[1]))
+		if err != nil {
+			fmt.Println(err)
+		}
+		if value, err := strconv.ParseFloat(row[2], 64); err == nil {
+			values[currentRow] = tsm1.NewValue(t.UnixNano(), value)
+			//fmt.Printf("%d: %v\n", t.UnixNano(), value)
+		}
+		currentRow += 1
+		if currentRow == size {
+			currentRow = 0
+			start := time.Now()
+			if b, err := tsm1.Values(values).Encode(nil); err == nil {
+				//fmt.Println(len(b))
+				totalSize += len(b)
+			}
+			elapsed := time.Since(start)
+			totalTime += elapsed
+		}
+	}
+
+	fmt.Printf("Total size: %v, Execution took %s\n", totalSize, totalTime)
+
+}
+
+func TestCompress_Size_Dust_Particulate_PM15Median(t *testing.T) {
+	size := 1000
+	layout := "01/02/2006 15:04:05"
+	values := make([]tsm1.Value, size)
+
+	f, err := os.Open("../../../NEON_size-dust-particulate-PM15Median.csv.gz")
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer gz.Close()
+	scanner := bufio.NewScanner(gz)
+	currentRow := 0
+	totalSize := 0
+	totalTime := time.Duration(0)
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ",")
+		t, err := time.Parse(layout, fmt.Sprintf("%s %s", row[0], row[1]))
+		if err != nil {
+			fmt.Println(err)
+		}
+		if value, err := strconv.ParseFloat(row[2], 64); err == nil {
+			values[currentRow] = tsm1.NewValue(t.UnixNano(), value)
+			//fmt.Printf("%d: %v\n", t.UnixNano(), value)
+		}
+		currentRow += 1
+		if currentRow == size {
+			currentRow = 0
+			start := time.Now()
+			if b, err := tsm1.Values(values).Encode(nil); err == nil {
+				//fmt.Println(len(b))
+				totalSize += len(b)
+			}
+			elapsed := time.Since(start)
+			totalTime += elapsed
+		}
+	}
+
+	fmt.Printf("Total size: %v, Execution took %s\n", totalSize, totalTime)
+
+}
+
+
+
+func TestCompress_Size_Dust_Particulate_PM15sub50RHMedian(t *testing.T) {
+	size := 1000
+	layout := "01/02/2006 15:04:05"
+	values := make([]tsm1.Value, size)
+
+	f, err := os.Open("../../../NEON_size-dust-particulate-PM15sub50RHMedian.csv.gz")
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer gz.Close()
+	scanner := bufio.NewScanner(gz)
+	currentRow := 0
+	totalSize := 0
+	totalTime := time.Duration(0)
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ",")
+		t, err := time.Parse(layout, fmt.Sprintf("%s %s", row[0], row[1]))
+		if err != nil {
+			fmt.Println(err)
+		}
+		if value, err := strconv.ParseFloat(row[2], 64); err == nil {
+			values[currentRow] = tsm1.NewValue(t.UnixNano(), value)
+			//fmt.Printf("%d: %v\n", t.UnixNano(), value)
+		}
+		currentRow += 1
+		if currentRow == size {
+			currentRow = 0
+			start := time.Now()
+			if b, err := tsm1.Values(values).Encode(nil); err == nil {
+				//fmt.Println(len(b))
+				totalSize += len(b)
+			}
+			elapsed := time.Since(start)
+			totalTime += elapsed
+		}
+	}
+
+	fmt.Printf("Total size: %v, Execution took %s\n", totalSize, totalTime)
+
+}
+
+func TestCompress_Size_Dust_Particulate_PM1Median(t *testing.T) {
+	size := 1000
+	layout := "01/02/2006 15:04:05"
+	values := make([]tsm1.Value, size)
+
+	f, err := os.Open("../../../NEON_size-dust-particulate-PM1Median.csv.gz")
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer gz.Close()
+	scanner := bufio.NewScanner(gz)
+	currentRow := 0
+	totalSize := 0
+	totalTime := time.Duration(0)
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ",")
+		t, err := time.Parse(layout, fmt.Sprintf("%s %s", row[0], row[1]))
+		if err != nil {
+			fmt.Println(err)
+		}
+		if value, err := strconv.ParseFloat(row[2], 64); err == nil {
+			values[currentRow] = tsm1.NewValue(t.UnixNano(), value)
+			//fmt.Printf("%d: %v\n", t.UnixNano(), value)
+		}
+		currentRow += 1
+		if currentRow == size {
+			currentRow = 0
+			start := time.Now()
+			if b, err := tsm1.Values(values).Encode(nil); err == nil {
+				//fmt.Println(len(b))
+				totalSize += len(b)
+			}
+			elapsed := time.Since(start)
+			totalTime += elapsed
+		}
+	}
+
+	fmt.Printf("Total size: %v, Execution took %s\n", totalSize, totalTime)
+
+}
+
+
+
+func TestCompress_Size_Dust_Particulate_PM1sub50RHMedian(t *testing.T) {
+	size := 1000
+	layout := "01/02/2006 15:04:05"
+	values := make([]tsm1.Value, size)
+
+	f, err := os.Open("../../../NEON_size-dust-particulate-PM1sub50RHMedian.csv.gz")
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer gz.Close()
+	scanner := bufio.NewScanner(gz)
+	currentRow := 0
+	totalSize := 0
+	totalTime := time.Duration(0)
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ",")
+		t, err := time.Parse(layout, fmt.Sprintf("%s %s", row[0], row[1]))
+		if err != nil {
+			fmt.Println(err)
+		}
+		if value, err := strconv.ParseFloat(row[2], 64); err == nil {
+			values[currentRow] = tsm1.NewValue(t.UnixNano(), value)
+			//fmt.Printf("%d: %v\n", t.UnixNano(), value)
+		}
+		currentRow += 1
+		if currentRow == size {
+			currentRow = 0
+			start := time.Now()
+			if b, err := tsm1.Values(values).Encode(nil); err == nil {
+				//fmt.Println(len(b))
+				totalSize += len(b)
+			}
+			elapsed := time.Since(start)
+			totalTime += elapsed
+		}
+	}
+
+	fmt.Printf("Total size: %v, Execution took %s\n", totalSize, totalTime)
+
+}
+
+func TestCompress_Size_Dust_Particulate_PM25Median(t *testing.T) {
+	size := 1000
+	layout := "01/02/2006 15:04:05"
+	values := make([]tsm1.Value, size)
+
+	f, err := os.Open("../../../NEON_size-dust-particulate-PM2.5Median.csv.gz")
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer gz.Close()
+	scanner := bufio.NewScanner(gz)
+	currentRow := 0
+	totalSize := 0
+	totalTime := time.Duration(0)
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ",")
+		t, err := time.Parse(layout, fmt.Sprintf("%s %s", row[0], row[1]))
+		if err != nil {
+			fmt.Println(err)
+		}
+		if value, err := strconv.ParseFloat(row[2], 64); err == nil {
+			values[currentRow] = tsm1.NewValue(t.UnixNano(), value)
+			//fmt.Printf("%d: %v\n", t.UnixNano(), value)
+		}
+		currentRow += 1
+		if currentRow == size {
+			currentRow = 0
+			start := time.Now()
+			if b, err := tsm1.Values(values).Encode(nil); err == nil {
+				//fmt.Println(len(b))
+				totalSize += len(b)
+			}
+			elapsed := time.Since(start)
+			totalTime += elapsed
+		}
+	}
+
+	fmt.Printf("Total size: %v, Execution took %s\n", totalSize, totalTime)
+
+}
+
+
+
+func TestCompress_Size_Dust_Particulate_PM25sub50RHMedian(t *testing.T) {
+	size := 1000
+	layout := "01/02/2006 15:04:05"
+	values := make([]tsm1.Value, size)
+
+	f, err := os.Open("../../../NEON_size-dust-particulate-PM2.5sub50RHMedian.csv.gz")
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer gz.Close()
+	scanner := bufio.NewScanner(gz)
+	currentRow := 0
+	totalSize := 0
+	totalTime := time.Duration(0)
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ",")
+		t, err := time.Parse(layout, fmt.Sprintf("%s %s", row[0], row[1]))
+		if err != nil {
+			fmt.Println(err)
+		}
+		if value, err := strconv.ParseFloat(row[2], 64); err == nil {
+			values[currentRow] = tsm1.NewValue(t.UnixNano(), value)
+			//fmt.Printf("%d: %v\n", t.UnixNano(), value)
+		}
+		currentRow += 1
+		if currentRow == size {
+			currentRow = 0
+			start := time.Now()
+			if b, err := tsm1.Values(values).Encode(nil); err == nil {
+				//fmt.Println(len(b))
+				totalSize += len(b)
+			}
+			elapsed := time.Since(start)
+			totalTime += elapsed
+		}
+	}
+
+	fmt.Printf("Total size: %v, Execution took %s\n", totalSize, totalTime)
+
+}
+
+func TestCompress_Size_Dust_Particulate_PM4Median(t *testing.T) {
+	size := 1000
+	layout := "01/02/2006 15:04:05"
+	values := make([]tsm1.Value, size)
+
+	f, err := os.Open("../../../NEON_size-dust-particulate-PM4Median.csv.gz")
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer gz.Close()
+	scanner := bufio.NewScanner(gz)
+	currentRow := 0
+	totalSize := 0
+	totalTime := time.Duration(0)
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ",")
+		t, err := time.Parse(layout, fmt.Sprintf("%s %s", row[0], row[1]))
+		if err != nil {
+			fmt.Println(err)
+		}
+		if value, err := strconv.ParseFloat(row[2], 64); err == nil {
+			values[currentRow] = tsm1.NewValue(t.UnixNano(), value)
+			//fmt.Printf("%d: %v\n", t.UnixNano(), value)
+		}
+		currentRow += 1
+		if currentRow == size {
+			currentRow = 0
+			start := time.Now()
+			if b, err := tsm1.Values(values).Encode(nil); err == nil {
+				//fmt.Println(len(b))
+				totalSize += len(b)
+			}
+			elapsed := time.Since(start)
+			totalTime += elapsed
+		}
+	}
+
+	fmt.Printf("Total size: %v, Execution took %s\n", totalSize, totalTime)
+
+}
+
+
+
+func TestCompress_Size_Dust_Particulate_PM4sub50RHMedian(t *testing.T) {
+	size := 1000
+	layout := "01/02/2006 15:04:05"
+	values := make([]tsm1.Value, size)
+
+	f, err := os.Open("../../../NEON_size-dust-particulate-PM4sub50RHMedian.csv.gz")
 	defer f.Close()
 	gz, err := gzip.NewReader(f)
 	if err != nil {
